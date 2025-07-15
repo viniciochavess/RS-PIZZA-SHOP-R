c51f118de0c2b096ba0754185069a755faf70c6e
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2025-07-15 11:29:17 -0300

    Commit: Logout automático

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,7 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, replace, useNavigate, useNavigation } from "react-router-dom";
 import { Headers } from "../Components/Headers";
+import { useEffect } from "react";
+import { api } from "@/utils/axios";
+import { AxiosError } from "axios";
 
 export function AppLayout() {
+  const navigation = useNavigate();
+  useEffect(() => {
+    const interceptionId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error instanceof AxiosError) {
+          const status = error.response?.status;
+          const code = error.response?.data.code;
+          if (status === 401 && code === "UNAUTHORIZED") {
+            navigation("/sign-in", { replace: true });
+          }
+        }
+      }
+    );
+    return () => {
+      api.interceptors.response.eject(interceptionId);
+    };
+  }, [navigation]);
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Headers />
